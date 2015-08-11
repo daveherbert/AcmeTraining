c86b27de01fa60c6e7ec6c866215fc81fd1ad03c
David Herbert <dherbert@sessiondigital.com>
2015-08-11 13:54:04 +0100

    Add block and layout

---
+++ app/code/community/Acme/Training/Block/MyBlock.php
@@ -0,0 +1,18 @@
+<?php
+
+class Acme_Training_Block_MyBlock extends Mage_Core_Block_Template
+{
+    public function getWeatherFor($city)
+    {
+        $client = new Client([
+            'base_uri' => 'http://api.openweathermap.org/data/2.5/',
+            'timeout' => 2.0
+        ]);
+
+        $response = $client->get('weather?q='.$city);
+        $contents = $response->getBody()->getContents();
+
+        return json_decode($contents);
+    }
+}
+
