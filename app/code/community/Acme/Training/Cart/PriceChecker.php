b652b2bb1346eb3cce2379ca27f880e98cf7a00d
David Herbert <dherbert@sessiondigital.com>
2015-08-11 15:42:57 +0100

    Log all the things

--- app/code/community/Acme/Training/Cart/PriceChecker.php
+++ app/code/community/Acme/Training/Cart/PriceChecker.php
@@ -2,9 +2,23 @@
 
 class Acme_Training_Cart_PriceChecker
 {
+
+    /**
+     * @var Mage_Core_Model_Logger
+     */
+    private $logger;
+
+    public function __construct(Mage_Core_Model_Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function filterCheapest(
         Acme_Training_Model_Resource_Product_Collection $collection
     ) {
+
+        $this->logger->log('filterCheapest Begin');
+
         foreach ($collection as $item) {
             $cheapest = $item;
             if ($item->getPrice() < $cheapest->getPrice()) {
@@ -16,5 +30,8 @@ class Acme_Training_Cart_PriceChecker
                 $collection->removeItemByKey($item->getId());
             }
         }
+
+        $this->logger->log('filterCheapest End');
+
     }
 }
